d7e41e4f5941ba10f2890a2dd3ebcf794fe5bbc7
jayzen <jayzen@foxmail.com>
2019-05-28 18:49:04 +0800

    do some fix

--- app/controllers/api/v1/wedding.rb
+++ app/controllers/api/v1/wedding.rb
@@ -4,9 +4,12 @@ module API
       include Default
       include Grape::Rails::Cache
 
+      before do
+        validate_appkey
+      end
+
       desc 'find basic'
       get '/wedding_basic' do
-        validate_appkey
         @wedding_basic = @user.wedding_basic
         wedding_basic = present @wedding_basic, with: API::Entities::WeddingBasic
         build_response code: 0, data: wedding_basic
@@ -14,7 +17,6 @@ module API
 
       desc 'find carousels'
       get '/carousels' do
-        validate_appkey
         @carousels = @user.carousels
         carousels = present @carousels, with: API::Entities::Carousel
         build_response code: 0, data: carousels
@@ -22,7 +24,6 @@ module API
 
       desc 'find one carousels'
       get '/first_carousel' do
-        validate_appkey
         @carousel = @user.carousels.first
         carousel = present @carousel, with: API::Entities::Carousel
         build_response code: 0, data: carousel
@@ -30,7 +31,6 @@ module API
 
       desc 'find photographs'
       get '/photographs' do
-        validate_appkey
         @photographs = @user.photographs
         photographs = present @photographs, with: API::Entities::Photograph
         build_response code: 0, data: photographs
@@ -38,7 +38,6 @@ module API
 
       desc 'find one photograph'
       get 'photographs/:id' do
-        validate_appkey
         @photograph = @user.photographs.find(params[:id])
         photograph = present @photograph, with: API::Entities::Photograph
         build_response code: 0, data: photograph
@@ -46,7 +45,6 @@ module API
 
       desc 'create consult'
       post '/consult' do
-        validate_appkey
         @consult = @user.consults.create(name: params[:name], contact: params[:contact])
         if @consult.errors.messages.size != 0
           error!({code: 102, error:  @consult.errors.messages.values.flatten.first})
@@ -55,7 +53,6 @@ module API
 
       desc 'create appointment'
       post '/appointment' do
-        validate_appkey
         @appointment = @user.appointments.create(name: params[:name], contact: params[:contact], date: params[:date], time: params[:time])
         if @appointment.errors.messages.size != 0
           error!({code: 102, error:  @appointment.errors.messages.values.flatten.first})
