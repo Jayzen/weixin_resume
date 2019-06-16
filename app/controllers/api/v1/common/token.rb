cf2d13dc04d7392d9bface82268b57fc72cfcbb9
jayzen <jayzen@foxmail.com>
2019-06-16 10:56:53 +0800

    do some fix

--- app/controllers/api/v1/common/token.rb
+++ app/controllers/api/v1/common/token.rb
@@ -12,7 +12,6 @@ module API
         post '/token/achieve' do
           validate_appkey
           openid_message = @user.get_openid(params[:code])
-          logger.info openid_message
           if openid_message["errcode"]
             error!({code: 1, message: 'opendid获取错误'})
           else
