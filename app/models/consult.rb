941d20a2ab51b6a98b9819a89b47a514fbe04dd3
jayzen <jayzen@foxmail.com>
2019-06-17 12:13:53 +0800

    do some fix

--- app/models/consult.rb
+++ app/models/consult.rb
@@ -4,5 +4,6 @@ class Consult < ApplicationRecord
 
   validates :name, presence: { message: "姓名不能为空" }
   validates :contact, presence: { message: "手机号码不能为空" }
+  validates :content, presence: { message: "咨询内容不能为空" }
   validates :contact, format: { with: VALID_TEL_REGEX, message: "手机号码格式不正确" }, unless: proc{ |consult| consult.contact.blank? }
 end
