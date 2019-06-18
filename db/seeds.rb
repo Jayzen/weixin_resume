662cc4e860bfef234f80ec7ead70fd749b469bf1
jayzen <jayzen@foxmail.com>
2019-06-18 18:17:48 +0800

    modify seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,12 @@
 #create user
-@user_one= User.create(email: "demo@demo.com", name: "demo", password: "111111", password_confirmation: "111111", roles: :root_admin)
-puts "finish add user demo"
+#@user_one= User.create(email: "demo@demo.com", name: "demo", password: "111111", password_confirmation: "111111", roles: :root_admin)
+#puts "finish add user demo"
 
 #set root_admin roles
-@user_one.roles = :root_admin
-@user_one.save
+#@user_one.roles = :root_admin
+#@user_one.save
 
-puts "finish add root_admin role to demo"
+#puts "finish add root_admin role to demo"
+@user = User.find_by(email: "wedding_one@demo.com")
+@user.roles = [:wedding_basic, :location, :carousel, :photograph, :consult, :top, :appointment, :recent, :home_photograph_category]
+@user.save
