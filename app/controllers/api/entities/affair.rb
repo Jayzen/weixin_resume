d1af266a93b6876de8a73be23966ed8b5bf4f745
jayzen <jayzen@foxmail.com>
2019-06-19 19:22:11 +0800

    do some fix

--- app/controllers/api/entities/affair.rb
+++ app/controllers/api/entities/affair.rb
@@ -3,6 +3,7 @@ module API
     class Affair < Grape::Entity
       expose :id
       expose :name
+      expose :updated_at
       expose :affair_images, using: API::Entities::AffairImage
     end
   end
