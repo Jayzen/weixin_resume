6c7449fcdaa0348ff585815ba0d5afa2f9f3df43
jayzen <jayzen@foxmail.com>
2019-06-19 19:35:51 +0800

    do some fix

--- app/controllers/api/entities/affair.rb
+++ app/controllers/api/entities/affair.rb
@@ -5,6 +5,11 @@ module API
       expose :name
       expose :updated_at
       expose :affair_images, using: API::Entities::AffairImage
+    
+      private
+        def updated_at
+          updated_at = object.updated_at.strftime("%F %H:%M")
+        end
     end
   end
 end
