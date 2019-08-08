bc3415bac25116d5d55bf6ae5a3acfbec6277305
jayzen <jayzen@foxmail.com>
2019-08-08 22:36:58 +0800

    add product comment and like

--- app/models/product_comment.rb
+++ app/models/product_comment.rb
@@ -3,4 +3,5 @@ class ProductComment < ApplicationRecord
   belongs_to :guest
 
   validates :content, presence: { message: "内容不能为空" }  
+  default_scope { order(created_at: :desc) }
 end
--- app/models/product_like.rb
+++ app/models/product_like.rb
@@ -1,4 +1,5 @@
 class ProductLike < ApplicationRecord
   belongs_to :product, counter_cache: true
   belongs_to :guest
+  default_scope { order(created_at: :desc) }
 end
